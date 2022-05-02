5a858deabc24cb48e47c2ab1e9ba7b9f910af82d
VinuH2020 <88569912+Vinu9975@users.noreply.github.com>
2022-05-02 15:33:59 +0530

    Ticket App In react

---
+++ TicketingApplaction_In_React/Client/src/app/components/Profile.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+import { Alert } from "react-bootstrap";
+import AppNavbar from './AppNavbar';
+import AuthenticationService from '../services/AuthenticationService';
+import FormDialog from './AddTicket.js'
+import {getTicket} from '../Store/TicketActions/TicketActions';
+import {connect} from 'react-redux';
+import BasicTable from './TicketList.js'
+import BasicTable1 from './singalSearch'
+
+class Profile extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {user: undefined};
+    this.state={ticket:undefined};
+  }
+
+  componentDidMount() {
+    const user = AuthenticationService.getCurrentUser();
+    this.setState({user: user});
+    this.props.getTicket();
+  }
+
+  render() {
+    let userInfo = "";
+    const user = this.state.user;
+    // login
+    if (user && user.accessToken) {
+      userInfo = (
+        <div >
+          <Alert >
+              <FormDialog  user={user}/>
+              <hr></hr>
+              {/* component for single searchbarr to search all field */}
+              {/* if you want use thise then comment bellow BasicTable component */}
+              {/* <BasicTable1 user={user}/> */}
+
+              {/* component for serch particuler field in columan that user want */}
+              {/* if you want use thise then comment above BasicTable1 component */}
+              <BasicTable user={user}/>   
+          </Alert>
+        </div>
+      );} else { 
+        // not login
+        userInfo =<div >
+                    <Alert variant="primary">
+                      <h5>Login For Add Ticket</h5>
+                      <Button color="success"><Link to="/signin"><span style={{color:"white"}}>Login</span></Link></Button>
+                    </Alert>
+                  </div>
+    }
+
+  return (
+    <div style={{margin:"0"}} id="profileDiv">
+      <AppNavbar/>
+      <Container fluid>
+        {userInfo}
+      </Container>
+    </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return{
+    ticket:state.tickets,
+  }
+}
+
+export default connect(mapStateToProps,{getTicket}) (Profile);
---
+++ TicketingApplaction_In_React/Server/controllers/Ticket.js
@@ -0,0 +1,75 @@
+const Ticket = require("../models/Ticket.js");
+var ObjectId=require('mongoose').Types.ObjectId;
+
+exports.add=async(req,res)=>{
+    try{
+        const ticket=await new Ticket(req.body)
+        await ticket.save((err,ticket)=>{
+            if(err){
+                return res.status(400).json({ err })
+            }
+            res.status(200).json({ ticket })
+        })
+    }catch(err){
+        console.error(err.message)
+    }
+}
+
+exports.send=async(req,res)=>{
+    Ticket.find((err,docs)=>{
+        if(err) throw err;
+        res.send(docs);
+     }
+    )
+    // const T=Ticket.find().sort({Date:-1});
+    // res.send(T);
+
+}
+
+exports.update=async(req,res)=>{
+    
+    var updateticket={
+        desc:req.body.desc,
+        UpdateDate:req.body.UpdateDate
+    };
+    Ticket.findByIdAndUpdate(req.params.id,{$set:updateticket},{new:true},(err,docs)=>{
+        if(err) throw err;
+        res.send(docs);
+    });
+}
+
+exports.deleteTicket=async(req,res)=>{
+    
+    var deleteticket={
+        DeleteDate:req.body.DeleteDate,
+        isDelete:req.body.isDelete
+    };
+    Ticket.findByIdAndUpdate(req.params.id,{$set:deleteticket},{new:true},(err,docs)=>{
+        if(err) throw err;
+        res.send(docs);
+    });
+}
+
+
+
+
+exports.getTicket=async(req,res)=>{
+    console.log("the ticket id from client  "+req.params.id)
+    
+    if(ObjectId.isValid(req.params.id))
+    {    
+        Ticket.findById(req.params.id,(err,docs)=>{
+            if(err) throw err;
+            console.log(docs)
+            res.send(docs);
+        });
+    }else
+    {
+        return res.status(200).send("No Record Found with Given Id: "+req.params.id);
+    }
+}
+
+
+
+
+   
